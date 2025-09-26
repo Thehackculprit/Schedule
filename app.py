b2044e86ef048548ff533e36fad0181d91332fe4
Hack Culprit <hackculprit@gmail.com>
2025-09-26 22:30:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -127,4 +127,6 @@ def admin_logout():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    print(f"Server running on port {port}")
+    app.run(host="0.0.0.0", port=port)
+
